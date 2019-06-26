157388fb5c474b94d3f9232215d03b2344449178
jal9626 <jal9626@gmail.com>
2019-06-25 21:50:02 -0400

    react testing project started

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,6 @@
 // Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import Dashboard from './Dashboard';
