80c41f5eb0473309f21914ebd48159ada521277a
jal9626 <jal9626@gmail.com>
2019-06-25 22:13:02 -0400

    snapshots created for all components

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import Control from './Control';
+
+describe('<Control />', () => {
+    it('matches snapshot', () => { // this test compares a snapshot of the code to the current code
+        const tree = renderer.create(<Control />); // generated a DOM tree
+ 
+        expect(tree.toJSON()).toMatchSnapshot();
+    }) 
+ })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -4,3 +4,12 @@ import renderer from 'react-test-renderer';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import Dashboard from './Dashboard';
+
+
+describe('<Dashboard />', () => {
+   it('matches snapshot', () => { // this test compares a snapshot of the code to the current code
+       const tree = renderer.create(<Dashboard />); // generated a DOM tree
+
+       expect(tree.toJSON()).toMatchSnapshot();
+   }) 
+})
